eef357fa796f270061464f484121e4d763d2aaeb
Santiago <santiroblesdiaz@gmail.com>
2023-12-05 15:41:01 -0300

    arregle cosas

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class pais(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+
+class cliente(models.Model):
+    nombre = models.CharField(max_length=100),
+    apellido = models.CharField(max_length=100),
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen = models.ForeignKey(pais, on_delete=models.SET_NULL, null=True, blank=True)
